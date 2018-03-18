e2740fffa307fe4254b14d78f1bb941d3d5440f1
Dustin Flanary <dflanary@ebay.com>
2018-03-17 19:52:33 -0700

    139 assessment

---
+++ exercises/medium_1/sandwich_code.rb
@@ -0,0 +1,10 @@
+class TextAnalyzer
+  def process
+    # your implementation
+  end
+end
+
+analyzer = TextAnalyzer.new
+analyzer.process { puts # your implementation }
+analyzer.process { puts # your implementation }
+analyzer.process { puts # your implementation }
